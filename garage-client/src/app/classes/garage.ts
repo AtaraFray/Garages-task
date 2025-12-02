6c7cd0d8fd898cfcf1e92247864e139ca940dc53
AtaraFray <al7139697@gmail.com>
2025-12-02 20:44:10 +0200

    Add garage class

---
+++ garage-client/src/app/classes/garage.ts
@@ -0,0 +1,20 @@
+export class Garage {
+
+    constructor(
+        public  Id : number,
+        public  GarageId :number,
+        public  GarageNumber :number,
+        public  GarageName :string,
+        public  GarageCodeType :number,
+        public  GarageType :string,
+        public  GarageAddress :string,
+        public  GarageCity :string,
+        public  GaragePhone :string,
+        public  GaragePostalCode:number,
+        public  GarageProfession :number,
+        public  Profession :string,
+        public  ProfessionManager :string,
+        public  RegisterCompany :number,
+        public  Testime :string
+    ){}
+}
